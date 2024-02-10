f8ba2703dc34352c3562c6b222beccfae338f046
dheerajmekala <dheeraj7596.dm@gmail.com>
2024-02-09 21:25:39 -0800

    added toolver

---
+++ data/toolenv/tools/toolver/sambanova/api.py
@@ -0,0 +1,29 @@
+import requests
+
+
+def forecast_air_pollution(lat: str, lon: str, api_key: str = '2a045ada80a040c4d68880e3abc55c5f'):
+    """
+    "Get the balance of an bitcoin address"
+
+    """
+    url = f"curl -X GET 'https://api.openweathermap.org/data/2.5/air_pollution/forecast?lat={lat}&lon={lon}&appid={api_key}'"
+    response = requests.get(url)
+    try:
+        observation = response.json()
+    except:
+        observation = response.text
+    return observation
+
+
+def current_air_pollution(lat: str, lon: str, api_key: str = '2a045ada80a040c4d68880e3abc55c5f'):
+    """
+    "Get the balance of an bitcoin address"
+
+    """
+    url = f"curl -X GET 'https://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={api_key}'"
+    response = requests.get(url)
+    try:
+        observation = response.json()
+    except:
+        observation = response.text
+    return observation
